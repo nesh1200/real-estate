469c0235d5e18fd122021292a350dea561e0332f
nesh1200 <jamoniberico11@gmail.com>
2021-12-01 18:50:55 +0200

    first commit

---
+++ src/main/java/com/example/quatro_to/QuatroToApplication.java
@@ -0,0 +1,13 @@
+package com.example.quatro_to;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuatroToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuatroToApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quatro_to/QuatroToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quatro_to;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuatroToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
